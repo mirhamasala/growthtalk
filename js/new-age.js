8f8e4b2b7afb0ae6b622e742473ccdfd3122e005
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-05-21 20:55:09 +0200

    aos jquery animation

--- js/new-age.js
+++ js/new-age.js
@@ -41,36 +41,3 @@
 
 })(jQuery); // End of use strict
 
-// SlideIn function
-$(window).scroll(function(event) {
-
-  $(".module").each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass("come-in");
-    }
-  });
-
-});
-
-var win = $(window);
-var allMods = $(".module");
-
-// Already visible modules
-allMods.each(function(i, el) {
-  var el = $(el);
-  if (el.visible(true)) {
-    el.addClass("already-visible");
-  }
-});
-
-win.scroll(function(event) {
-
-  allMods.each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass("come-in");
-    }
-  });
-
-});
