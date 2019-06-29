2a22c50cb36303ecb8d9d964e102376b5c09348d
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-06-29 17:45:54 +0200

    progress on CF integration

--- js/conversational-form.js
+++ js/conversational-form.js
@@ -2,9 +2,9 @@ window.onload = function() {
   var conversationalForm = window.cf.ConversationalForm.startTheConversation({
     formEl: document.getElementById("form"),
     context: document.getElementById("cf-context"),
-    submitCallback: function() {
-     conversationalForm.addRobotChatResponse("Alright, you are done.");
-    }
+    userImage: "text:🧑",
+    preventAutoFocus: true,
+    hideUserInputOnNoneTextInput: true
   });
 };
 
