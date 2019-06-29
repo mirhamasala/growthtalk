0781ae18270e230f57c451c31052a971e888b5aa
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-06-30 01:07:59 +0200

    cf submit

--- js/conversational-form.js
+++ js/conversational-form.js
@@ -3,13 +3,6 @@ window.onload = function() {
     formEl: document.getElementById("form"),
     context: document.getElementById("cf-context"),
     userImage: "text:🧑",
-    hideUserInputOnNoneTextInput: true,
-    submitCallback: function() {
-        var formData = conversationalForm.getFormData();
-        var formDataSerialized = conversationalForm.getFormData(true);
-        console.log("Formdata:", formData);
-        console.log("Formdata, serialized:", formDataSerialized);
-        conversationalForm.addRobotChatResponse("Alright, you are done.")
-      }
+    hideUserInputOnNoneTextInput: true
   });
 };
