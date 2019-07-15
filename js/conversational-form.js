b6326133706d4ccfb76e66076ea91aaf3af8ec86
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-07-15 11:26:48 +0200

    ajax submitcallback

--- js/conversational-form.js
+++ js/conversational-form.js
@@ -3,6 +3,14 @@ window.onload = function() {
     formEl: document.getElementById("form"),
     context: document.getElementById("cf-context"),
     userImage: "text:🧑",
-    hideUserInputOnNoneTextInput: true
+    hideUserInputOnNoneTextInput: true,
+    submitCallback: function(){
+        // be aware that this prevents default form submit.
+        var formData = conversationalForm.getFormData();
+        var formDataSerialized = conversationalForm.getFormData(true);
+        console.log("Formdata:", formData);
+        console.log("Formdata, serialized:", formDataSerialized);
+        conversationalForm.addRobotChatResponse("Awesome! My partners in crime Andreea and Maxime will get back to you within 24h! 🎉");
+    }
   });
 };
