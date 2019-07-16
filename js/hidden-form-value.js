6968af7710afe649edab2119218f0337957303df
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-07-16 11:42:27 +0200

    added hidden values in form submission

---
+++ js/hidden-form-value.js
@@ -0,0 +1,42 @@
+$(function() {
+  $('#cta-curious').click(function(){
+    $("#form").append('<input type="hidden" name="navbar-curious" value="navbar-curious" />')
+  });
+});
+
+$(function() {
+  $('#header-curious').click(function(){
+    $("#form").append('<input type="hidden" name="header-curious" value="header-curious" />')
+  });
+});
+
+$(function() {
+  $('#quick-chat').click(function(){
+    $("#form").append('<input type="hidden" name="quick-chat" value="quick-chat" />')
+  });
+});
+
+$(function() {
+  $('#share-knowledge').click(function(){
+    $("#form").append('<input type="hidden" name="share-knowledge" value="share-knowledge" />')
+  });
+});
+
+$(function() {
+  $('#basic').click(function(){
+    $("#form").append('<input type="hidden" name="basic" value="basic" />')
+  });
+});
+
+
+$(function() {
+  $('#premium').click(function(){
+    $("#form").append('<input type="hidden" name="premium" value="premium" />')
+  });
+});
+
+$(function() {
+  $('#sky').click(function(){
+    $("#form").append('<input type="hidden" name="sky is the limit" value="sky is the limit" />')
+  });
+});
