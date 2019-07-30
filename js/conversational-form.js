5a7bdc163b3eb71ecf99513328d1a2b2eee7bc31
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-07-30 09:21:08 +0200

    removed tawk.to on mobile and added scroll to top button instead

--- js/conversational-form.js
+++ js/conversational-form.js
@@ -7,4 +7,11 @@ window.onload = function() {
   });
 };
 
-
+window.onload = function() {
+  var conversationalForm = window.cf.ConversationalForm.startTheConversation({
+    formEl: document.getElementById("form-2"),
+    context: document.getElementById("cf-context-2"),
+    userImage: "text:🧑",
+    hideUserInputOnNoneTextInput: true
+  });
+};
