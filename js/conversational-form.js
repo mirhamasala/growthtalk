ba2f69f601e667dfe1767b97dd198710845494f9
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-07-30 09:45:21 +0200

    fixed cf bug

--- js/conversational-form.js
+++ js/conversational-form.js
@@ -6,12 +6,3 @@ window.onload = function() {
     hideUserInputOnNoneTextInput: true
   });
 };
-
-window.onload = function() {
-  var conversationalForm = window.cf.ConversationalForm.startTheConversation({
-    formEl: document.getElementById("form-2"),
-    context: document.getElementById("cf-context-2"),
-    userImage: "text:🧑",
-    hideUserInputOnNoneTextInput: true
-  });
-};
