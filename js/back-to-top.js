f97f0f48cc5b137378efd5df203f309ef328fd0d
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-08-03 11:37:03 +0200

    added scroll to top button on modals

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -18,3 +18,45 @@ $(document).ready(function () {
     return false;
   });
 });
+
+// Back to Top button behaviour on manifesto modal
+$(document).ready(function () {
+  var pxShow = 50;
+  var scrollSpeed = 500;
+  $('#manif-modal').on('scroll', function() {
+    if ($('#manif-modal').scrollTop() >= pxShow) {
+       $("#backtotop").removeClass('display');
+       $("#backtotop").addClass('visibility');
+    } else {
+       $("#backtotop").removeClass('visibility');
+    }
+  });
+
+  $('#backtotop a').on('click', function() {
+    $('#manif-modal').animate({
+       scrollTop: 0
+    }, scrollSpeed);
+    return false;
+  });
+});
+
+// Back to Top button behaviour on topics modal
+$(document).ready(function () {
+  var pxShow = 50;
+  var scrollSpeed = 500;
+  $('#topics-modal').on('scroll', function() {
+    if ($('#topics-modal').scrollTop() >= pxShow) {
+       $("#backtotop").removeClass('display');
+       $("#backtotop").addClass('visibility');
+    } else {
+       $("#backtotop").removeClass('visibility');
+    }
+  });
+
+  $('#backtotop a').on('click', function() {
+    $('#topics-modal').animate({
+       scrollTop: 0
+    }, scrollSpeed);
+    return false;
+  });
+});
