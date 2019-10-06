74cf985b06ae99416e93c86c1feffa698b65afad
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-10-06 19:13:11 -0400

    starting styling text playbook

---
+++ js/smooth-scroll.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  console.log('hello');
+  // var hashTagActive = "";
+  // $(".scroll").on("click touchstart" , function (event) {
+  //     if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+  //         event.preventDefault();
+  //         //calculate destination place
+  //         var dest = 0;
+  //         if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+  //             dest = $(document).height() - $(window).height();
+  //         } else {
+  //             dest = $(this.hash).offset().top;
+  //         }
+  //         //go to destination
+  //         $('html,body').animate({
+  //             scrollTop: dest
+  //         }, 2000, 'swing');
+  //         hashTagActive = this.hash;
+  //     }
+  // });
+
+  $('#toc .nav-link').click(function(){
+    console.log('hello');
+    $(this).addClass("scroll");
+  });
+});
+
